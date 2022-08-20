1d00b73852d4dc12e6707aaad11a6f76f503a32b
David Henderson <flynnhendy@gmail.com>
2022-08-21 00:56:51 +0200

    add firebase packages

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,5 +3,7 @@
  *
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
-
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/functions'
 // You can delete this file if you're not using it
